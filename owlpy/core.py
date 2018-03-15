c994eb0cc18603c958dc9393456cfc88d880bd8b
Antonio Vilches <antonio.vilches@gcatsoft.com>
2018-03-15 20:24:04 +0100

    Changing the implementation of some methods to yield further performance. I have reimplemented some parts of the code to rely on numpy, which is already vectorised and makes a more efficient usage of the CPU resources. It shows a speedup between 3 and 4x.

--- owlpy/core.py
+++ owlpy/core.py
@@ -13,145 +13,151 @@ import time
 
 import matplotlib.pyplot as plt
 
-def slidingDotProduct(Q, T):
-    n = len(T)
-    m = len(Q)
 
-    Ta = list(T) # Copy the T without modifying the original
-    [Ta.append(0) for i in range(0, n)]
-    
-    Qr = Q[::-1]
-    
-    Qra = list(Qr) # Copy the T without modifying the original
-    [Qra.append(0) for i in range(0, 2*n - m)]
-    
-    Qraf = np.fft.fft(Qra)
-    Taf = np.fft.fft(Ta)
-    
-    QT = np.fft.ifft(np.multiply(Qraf, Taf))
-    return QT[m:n]
-    
-def calculateDistanceProfile(QT, Q, T, sumT, sumT2, sumQ, sumQ2, meanT, meanT_2, sigmaT, sigmaT2):
-    n = len(T)
-    m = len(Q)
+def slidingDotProduct(q, t):
+    n = t.size
+    m = q.size
 
-    # computing the distances -- O(n) time.
+    # Append t with n zeros
+    ta = np.append(t, np.zeros(n))
 
-    a = [(sumT2[i] - 2*sumT[i]*meanT[i] + m*meanT_2[i])/sigmaT2[i] for i in range(0, n - m)]
-    b = [-2*(QT[i] - sumQ*meanT[i])/sigmaT[i] for i in range(0, n - m)]
-    c = sumQ2
-    dist = [a[i] + b[i] + c for i in range(0, n - m)]
-    D = np.abs(np.sqrt(dist))
-    return D
+    # Reverse Q
+    qr = np.flip(q, 0)
+
+    # Append qra
+    qra = np.append(qr, np.zeros(2 * n - m))
+
+    # Compute FFTs
+    qraf = np.fft.fft(qra)
+    taf = np.fft.fft(ta)
+
+    # Compute the inverse FFT to the element-wise multiplication of qraf and taf
+    qt = np.fft.ifft(np.multiply(qraf, taf))
+    return qt[m:n]
+
+
+def calculateDistanceProfile(q, t, qt, a, sum_q, sum_q2, mean_t, sigma_t):
+    n = t.size
+    m = q.size
+
+    b = np.zeros(n - m)
+    dist = np.zeros(n - m)
+    for i in range(0, n - m):
+        b[i] = -2 * (qt[i].real - sum_q * mean_t[i]) / sigma_t[i]
+        dist[i] = a[i] + b[i] + sum_q2
+    return np.sqrt(np.abs(dist))
+
 
 # The code below takes O(m) for each subsequence
 # you should replace it for MASS
-def computeMeanStd(Q, T):
-    n = len(T)
-    m = len(Q)
-
+def computeMeanStdForQuery(Q):
     # Compute Q stats -- O(n)
     sumQ = np.sum(Q)
     sumQ2 = np.sum(np.power(Q, 2))
+    return sumQ, sumQ2
 
-    # Compute T stats -- O(n)
-    cum_sumT = np.cumsum(T, dtype=float)
-    cum_sumT2 = np.cumsum(np.power(T, 2), dtype=float)
-    sumT2 = [cum_sumT2[i + m] - cum_sumT2[i] for i in range(0, n - m)]
-    sumT = [cum_sumT[i + m] - cum_sumT[i] for i in range(0, n - m)]
-    meanT = [sumT[i]/m for i in range(0, n - m)]
-    meanT2 = [sumT2[i]/m for i in range(0, n - m)]
-    meanT_2 = np.power(meanT, 2)
-    sigmaT2 = [meanT2[i] - meanT_2[i] for i in range(0,n - m)]
-    sigmaT = np.sqrt(sigmaT2)
-    
-    return sumT, sumT2, sumQ, sumQ2, meanT, meanT_2, sigmaT, sigmaT2
-    
+
+def preComputeMeanStdForTS(ta, m):
+    na = len(ta)
+    sum_t = np.zeros(na - m)
+    sum_t2 = np.zeros(na - m)
+
+    # Compute the stats for t
+    cumulative_sum_t = np.cumsum(ta)
+    cumulative_sum_t2 = np.cumsum(np.power(ta, 2))
+    for i in range(na - m):
+        sum_t[i] = cumulative_sum_t[i + m] - cumulative_sum_t[i]
+        sum_t2[i] = cumulative_sum_t2[i + m] - cumulative_sum_t2[i]
+    mean_t = np.divide(sum_t, m)
+    mean_t2 = np.divide(sum_t2, m)
+    mean_t_p2 = np.power(mean_t, 2)
+    sigma_t2 = np.subtract(mean_t2, mean_t_p2)
+    sigma_t = np.sqrt(sigma_t2)
+    return sum_t, sum_t2, mean_t, mean_t2, mean_t_p2, sigma_t, sigma_t2
+
+
 # MUEEN’S ALGORITHM FOR SIMILARITY SEARCH (MASS)
-def mass(Q, T):
-    if np.std(Q)!=0:
-        Q = (Q -np.mean(Q))/np.std(Q)
+def mass(Q, T, a, meanT, sigmaT):
+    # Z-Normalisation
+    if np.std(Q) != 0:
+        Q = (Q - np.mean(Q)) / np.std(Q)
     QT = slidingDotProduct(Q, T)
-    sumT, sumT2, sumQ, sumQ2, meanT, meanT_2, sigmaT, sigmaT2 = computeMeanStd(Q, T)
-    D = calculateDistanceProfile(QT, Q, T, sumT, sumT2, sumQ, sumQ2, meanT, meanT_2, sigmaT, sigmaT2)
-    
-    return D
+    sumQ, sumQ2 = computeMeanStdForQuery(Q)
+    return calculateDistanceProfile(Q, T, QT, a, sumQ, sumQ2, meanT, sigmaT)
 
-def elementWiseMin(Pab, Iab, D, idx,ignore_trivial,m):
 
-    Pab_copy = [1000000 for i in range(0, len(Pab))]
-    flag = False
-
+def elementWiseMin(Pab, Iab, D, idx, ignore_trivial, m):
     for i in range(0, len(D)):
-        if not ignore_trivial or (np.abs(idx-i)>m):
-            if (Pab[i] == float('Inf')):
-                Pab_copy[i] = D[i]
-                Iab[i] = idx
-                flag = True
-            elif (D[i] < Pab[i]):
+        if not ignore_trivial or (np.abs(idx - i) > m):
+            if D[i] < Pab[i]:
                 Pab[i] = D[i]
                 Iab[i] = idx
+    return Pab, Iab
 
-    if flag:
-        return Pab_copy, Iab
-    else:
-        return Pab, Iab
-    
-    
 
 def stamp(Ta, Tb, m):
     nb = len(Tb)
     na = len(Ta)
-    Pab = [float('Inf') for i in range(0, na - m)]
-    Iab = [0 for i in range(0, na - m)]
+    Pab = np.full(nb - m, float('Inf'))
+    Iab = np.full(nb - m, 0)
     idxes = range(0, nb - m)
-    
+
+    sumT, sumT2, meanT, meanT_2, meanTP2, sigmaT, sigmaT2 = preComputeMeanStdForTS(Ta, m)
+
+    a = np.zeros(na - m)
+    for i in range(0, na - m):
+        a[i] = (sumT2[i] - 2 * sumT[i] * meanT[i] + m * meanTP2[i]) / sigmaT2[i]
+
     for idx in idxes:
-        D = mass(Tb[idx : idx + m], Ta)
-        Pab, Iab = elementWiseMin(Pab, Iab, D, idx, ignore_trivial=(Ta==Tb).all(),m=m)
-        
+        D = mass(Tb[idx: idx + m], Ta, a, meanT, sigmaT)
+        Pab, Iab = elementWiseMin(Pab, Iab, D, idx, ignore_trivial=(Ta == Tb).all(), m=m)
+
     return Pab, Iab
+
 
 # Quick Test
 def test_stamp(Ta, Tb, m):
     start_time = time.time()
-    
-        
+
     Pab, Iab = stamp(Ta, Tb, m)
     print("--- %s seconds ---" % (time.time() - start_time))
-    
-    plot_graphics(Ta,Tb,Pab, Iab,m)
+
+    plot_graphics(Ta, Tb, Pab, Iab, m)
     return Pab
-    
-def plot_graphics(Ta,Tb,values,indexes,m):
+
+
+def plot_graphics(Ta, Tb, values, indexes, m):
     fig_width = 16
     fig_height = 8
     fig_dpi = 100
     plt.figure(figsize=(fig_width, fig_height), dpi=fig_dpi)
+
     plt.subplot(411)
     plt.plot(Ta)
-    plt.xlim((0,len(Tb)))
-
+    plt.xlim((0, len(Ta)))
     plt.title('A')
+
     plt.subplot(412)
     plt.plot(Tb)
-    plt.plot(range(np.argmax(values),np.argmax(values)+m),Tb[np.argmax(values):np.argmax(values)+m],c='r')
-
+    plt.plot(range(np.argmax(values), np.argmax(values) + m), Tb[np.argmax(values):np.argmax(values) + m], c='r')
     plt.title('B')
-    plt.xlim((0,len(Tb)))
+    plt.xlim((0, len(Tb)))
 
     plt.subplot(413)
     plt.title('P_ab')
-    plt.plot(range(0,len(values)), values, '#ff5722')
-    plt.plot(np.argmax(values),np.max(values),marker='x',ms=10)
-    plt.xlim((0,len(Tb)))
+    plt.plot(range(0, len(values)), values, '#ff5722')
+    plt.plot(np.argmax(values), np.max(values), marker='x', ms=10)
+    plt.xlim((0, len(Ta)))
     plt.xlabel('Index')
     plt.ylabel('Value')
-    plt.subplot(414)    
-    plt.title('P_ab')
-    plt.plot(range(0,len(indexes)), indexes, '#ff5722')
+
+    plt.subplot(414)
+    plt.title('I_ab')
+    plt.plot(range(0, len(indexes)), indexes, '#ff5722')
     plt.xlabel('Index')
     plt.ylabel('Value')
+    plt.xlim((0, len(Ta)))
     plt.show()
+
 
 
